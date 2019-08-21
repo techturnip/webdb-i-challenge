6dc922b9cdc93cf2b3d2f2379bf15e4d22c1aee6
Tyler Turnipseed <tyler@techturnip.us>
2019-08-21 19:49:14 -0400

    setup server.js, add comments, setup accounts-router.jsclear

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -1,7 +1,25 @@
-const express = require('express')
-
+// IMPORTS/INITIALIZATION =========================|
+// ================================================|
+// import express router --------------------------|
+const router = require('express').Router()
+// bring in DB ------------------------------------|
 const db = require('../data/dbConfig.js')
-
-const router = express.Router()
-
+// ------------------------------------------------|
+// REQ HANDLERS ===================================|
+// ================================================|
+// Base URL '/api/accounts/' ----------------------|
+// ------------------------------------------------|
+// GET Request returns an array of all actions ----|
+router.get('/', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      console.log(accounts)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+// ------------------------------------------------|
+// EXPORT ROUTER ==================================|
+// ================================================|
 module.exports = router
