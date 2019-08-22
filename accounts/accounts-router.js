51df93355a10efc29b410b5f4af90669b334f824
Tyler Turnipseed <tyler@techturnip.us>
2019-08-21 20:10:03 -0400

    created GET and POST endpoints to get all accounts and create new accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -1,22 +1,41 @@
 // IMPORTS/INITIALIZATION =========================|
 // ================================================|
-// import express router --------------------------|
-const router = require('express').Router()
 // bring in DB ------------------------------------|
 const db = require('../data/dbConfig.js')
+// import express router --------------------------|
+const router = require('express').Router()
 // ------------------------------------------------|
 // REQ HANDLERS ===================================|
 // ================================================|
 // Base URL '/api/accounts/' ----------------------|
 // ------------------------------------------------|
-// GET Request returns an array of all actions ----|
+// GET Request returns an array of all accounts ---|
 router.get('/', (req, res) => {
   db('accounts')
     .then(accounts => {
-      console.log(accounts)
+      res.status(200).json(accounts)
     })
     .catch(err => {
       console.log(err)
+      res.status(500).json({ message: 'Failed to get accounts from database' })
+    })
+})
+// POST Request adds account to database ----------|
+router.post('/', (req, res) => {
+  const accountData = req.body
+  console.log(accountData)
+
+  db('accounts')
+    .insert(accountData)
+    .then(account => {
+      console.log(account)
+      res
+        .status(201)
+        .json({ message: `Account ${account} successfully created` })
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ message: 'Failed to insert new account' })
     })
 })
 // ------------------------------------------------|
